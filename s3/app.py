c029b126acb36e47f956622c405fc2ef26145005
Yogesh Choudhary <yca426@sfu.ca>
2022-03-11 02:15:49 -0800

    Updated playlist service and changes to db service

--- s3/app.py
+++ s3/app.py
@@ -42,19 +42,7 @@ db = {
     ]
 }
 
-NOT_AUTHENTICATED = Response(
-    json.dumps({"error": "missing auth"}),
-    status=401,
-    mimetype='application/json'
-)
-
-
-@bp.route('/', methods=['GET'])
-@metrics.do_not_track()
-def hello_world():
-    return ("If you are reading this in a browser, your service is "
-            "operational. Switch to curl/Postman/etc to interact using the "
-            "other HTTP verbs.")
+NOT_AUTHENTICATED = 'Missing Auth'
 
 
 @bp.route('/health')
@@ -63,51 +51,63 @@ def health():
     return Response("", status=200, mimetype="application/json")
 
 
-@bp.route('/readiness')
-@metrics.do_not_track()
-def readiness():
-    return Response("", status=200, mimetype="application/json")
-
-
 def get_user_from_auth(headers):
-    return None
+    if 'Authorization' not in headers:
+        raise(Exception(NOT_AUTHENTICATED))
+    decoded = jwt.decode(headers['Authorization'],'secret',algorithms=['HS256'])
+    return decoded['user_id']
 
 
 def get_playlist_from_id(id, headers):
     user_id = get_user_from_auth(headers)
     if not user_id:
-        return NOT_AUTHENTICATED
-    payload = {"objtype": "playlist", "objkey": id, "uid": user_id}
+        raise(Exception(NOT_AUTHENTICATED))
+    payload = {"objtype": "playlist", "objkey": id}
     url = db['name'] + '/' + db['endpoint'][0]
-    response = requests.get(url, params=payload)
-    if len(response.json()) == 0:
-        return Response("Playlist not found for this user", status=404, mimetype="application/json")
-    playlist_data = response.json()
+    response = requests.get(url, params=payload).json()
+
+    if len(response['Items']) == 0:
+        raise(Exception("Playlist not found for this user"))
+    playlist_data = response['Items'][0]
+    return (playlist_data)
+
+
+def get_detailed_playlist_from_id(id, headers):
+    playlist_data = get_playlist_from_id(id, headers)
+
     music_list = playlist_data['music_list'][:] # make a new copy.
     payload = {"objtype": "music"}
     url = db['name'] + '/' + db['endpoint'][4]
-    response = requests.get(url, params=payload)
-    playlist_data['music_list'] = list()
-    for song in response.json():
-        if song['objkey'] in music_list:
-            playlist_data['music_list'].append(song)
+    songs = requests.get(url, params=payload).json()['Items']
+
+    playlist_data['music_list'] = list(filter(lambda song: (song['music_id'] in music_list), songs))
     return (playlist_data)
 
 
 @bp.route('/all', methods=['GET'])
 def list_playlists():
-    user_id = get_user_from_auth(request.headers)
-    if not user_id:
-        return NOT_AUTHENTICATED
-    payload = {"objtype": "playlist", "uid": user_id}
-    url = db['name'] + '/' + db['endpoint'][0]
-    response = requests.get(url, params=payload)
-    return (response.json())
+    try:
+        user_id = get_user_from_auth(request.headers)
+    except Exception as ex:
+        return Response(str(ex), status=400, mimetype="application/json")
+    payload = {"objtype": "playlist"}
+    url = db['name'] + '/' + db['endpoint'][4]
+    response = requests.get(url, params=payload).json()['Items']
+    playlists = list(filter(lambda x: (x['uid'] == user_id), response))
+    return ({'playlists' : playlists})
 
 
 @bp.route('/<playlist_id>', methods=['GET'])
 def get_playlist(playlist_id):
-    return get_playlist_from_id(playlist_id, request.headers)
+    try:
+        user_id = get_user_from_auth(request.headers)
+        playlist = get_detailed_playlist_from_id(playlist_id, request.headers)
+    except Exception as ex:
+        return Response(str(ex), status=400, mimetype="application/json")
+    
+    if playlist['uid'] != user_id:
+        return Response('Not authorized to access this playlist', status=401, mimetype="application/json")
+    return (playlist)
 
 
 @bp.route('/', methods=['POST'])
@@ -115,15 +115,14 @@ def create_playlist():
     """
     Create a playlist.
     """
-    user_id = get_user_from_auth(request.headers)
-    if not user_id:
-        return NOT_AUTHENTICATED
     try:
+        user_id = get_user_from_auth(request.headers)
+
         content = request.get_json()
         playlist_name = content['playlist_name']
         music_list = content['music_list']
-    except Exception:
-        return json.dumps({"message": "error reading arguments"})
+    except Exception as ex:
+        return Response(str(ex), status=400, mimetype="application/json")
     url = db['name'] + '/' + db['endpoint'][1]
     response = requests.post(
         url,
@@ -134,95 +133,72 @@ def create_playlist():
     return (response.json())
 
 
-@bp.route('/add_song', methods=['POST'])
+@bp.route('/add_songs', methods=['PUT'])
 def add_songs_to_playlist():
     """
     Add songs to a playlist.
     """
     try:
+        user_id = get_user_from_auth(request.headers)
+
         content = request.get_json()
         playlist_id = content['playlist_id']
         music_list = content['music_list']
-    except Exception:
-        return json.dumps({"message": "error reading arguments"})
-    
-    playlist_data = get_playlist_from_id(playlist_id, request.headers)
-    if playlist_data == NOT_AUTHENTICATED: return playlist_data
-    # we have playlist data. now update the music list
-    user_id = get_user_from_auth(request.headers)
 
-    updated_music_list = list(set(music_list.append(playlist_data['music_list'])))
+        playlist_data = get_playlist_from_id(playlist_id, request.headers)
+    except Exception as ex:
+        return Response(str(ex), status=400, mimetype="application/json")
     
-    url = db['name'] + '/' + db['endpoint'][1]
+    # check if song exists in music
+    payload = {"objtype": "music"}
+    url = db['name'] + '/' + db['endpoint'][4]
+    songs = requests.get(url, params=payload).json()['Items']
+    all_music_ids = list()
+    for song in songs:
+        all_music_ids.append(songs['music_id'])
+
+    if not all(music_id in all_music_ids  for music_id in music_list):
+        return Response('One or more music IDs don\'t exist', status=400, mimetype="application/json")
+
+    music_list.extend(playlist_data['music_list'])
+    new_music_list = list(set(music_list))
+
+    url = db['name'] + '/' + db['endpoint'][3]
     response = requests.put(
         url,
         json={"objtype": "playlist",
               "objkey": playlist_id,
-              "music_list": updated_music_list,
-              "uid": user_id})
+              "music_list": new_music_list})
     return (response.json())
 
-'''
-@bp.route('/<playlist_id>', methods=['PUT'])
-def update_user(playlist_id):
-    headers = request.headers
-    # check header here
-    if 'Authorization' not in headers:
-        return Response(json.dumps({"error": "missing auth"}), status=401,
-                        mimetype='application/json')
+
+@bp.route('/delete_songs', methods=['PUT']) # Check if all music_id exist before adding
+def delete_songs_from_playlist():
+    """
+    Add songs to a playlist.
+    """
     try:
+        user_id = get_user_from_auth(request.headers)
+
         content = request.get_json()
-        email = content['email']
-        fname = content['fname']
-        lname = content['lname']
-    except Exception:
-        return json.dumps({"message": "error reading arguments"})
+        playlist_id = content['playlist_id']
+        music_list = content['music_list']
+
+        playlist_data = get_playlist_from_id(playlist_id, request.headers)
+    except Exception as ex:
+        return Response(str(ex), status=400, mimetype="application/json")
+    # we have playlist data. now update the music list
+
+    new_music_list = list(filter(lambda song: (song not in music_list),playlist_data['music_list']))
+
     url = db['name'] + '/' + db['endpoint'][3]
     response = requests.put(
         url,
-        params={"objtype": "user", "objkey": user_id},
-        json={"email": email, "fname": fname, "lname": lname})
+        json={"objtype": "playlist",
+              "objkey": playlist_id,
+              "music_list": new_music_list})
     return (response.json())
 
-
-@bp.route('/', methods=['POST'])
-def create_user():
-    """
-    Create a user.
-    If a record already exists with the same fname, lname, and email,
-    the old UUID is replaced with a new one.
-    """
-    try:
-        content = request.get_json()
-        lname = content['lname']
-        email = content['email']
-        fname = content['fname']
-    except Exception:
-        return json.dumps({"message": "error reading arguments"})
-    url = db['name'] + '/' + db['endpoint'][1]
-    response = requests.post(
-        url,
-        json={"objtype": "user",
-              "lname": lname,
-              "email": email,
-              "fname": fname})
-    return (response.json())
-
-
-@bp.route('/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    headers = request.headers
-    # check header here
-    if 'Authorization' not in headers:
-        return Response(json.dumps({"error": "missing auth"}),
-                        status=401,
-                        mimetype='application/json')
-    url = db['name'] + '/' + db['endpoint'][2]
-
-    response = requests.delete(url,
-                               params={"objtype": "user", "objkey": user_id})
-    return (response.json())
-'''
 
 # All database calls will have this prefix.  Prometheus metric
 # calls will not---they will have route '/metrics'.  This is
