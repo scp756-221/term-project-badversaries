a6dd9cb388f4c30feb768cdf1e31be80231660fc
Jiawen Zhang <jza289@sfu.ca>
2022-03-17 03:36:41 -0700

    Add list_all routine

--- s2/v1/app.py
+++ s2/v1/app.py
@@ -20,13 +20,6 @@ import requests
 
 import simplejson as json
 
-# Local modules
-import unique_code
-
-# The unique exercise code
-# The EXER environment variable has a value specific to this exercise
-ucode = unique_code.exercise_hash(os.getenv('EXER'))
-
 # The application
 
 app = Flask(__name__)
@@ -67,7 +60,13 @@ def list_all():
                         status=401,
                         mimetype='application/json')
     # list all songs here
-    return {}
+    payload = {"objtype": "music"}
+    url = db['name'] + '/' + db['endpoint'][3]
+    response = requests.get(
+        url,
+        params=payload,
+        headers={'Authorization': headers['Authorization']}).json()['Items']
+    return (response)
 
 
 @bp.route('/<music_id>', methods=['GET'])
@@ -135,7 +134,6 @@ if __name__ == '__main__':
         logging.error("missing port arg 1")
         sys.exit(-1)
 
-    app.logger.error("Unique code: {}".format(ucode))
     p = int(sys.argv[1])
     # Do not set debug=True---that will disable the Prometheus metrics
     app.run(host='0.0.0.0', port=p, threaded=True)
